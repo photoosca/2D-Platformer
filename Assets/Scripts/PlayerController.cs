9b250e22e867b1f26a3e90791363ceb503c5933b
photoosca <88528506+photoosca@users.noreply.github.com>
2021-10-01 17:49:52 +0700

    ukti Belajar Pert - 09
    ukti Belajar Pert - 09 is done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,31 +5,40 @@ using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-bool isJump = true;
-bool isDead = false;
-int idMove = 0;
-Animator anim;
+    bool isJump = true;
+    bool isDead = false;
+    int idMove = 0;
+    Animator anim;
+    public GameObject Projectile; 
+    public Vector2 projectileVelocity; 
 
-// Use this for initialization
+    public Vector2 projectileOffset; 
+    public float cooldown = 0.5f;
+    bool isCanShoot = true; // memastikan untuk kapan dapat menembak
+
+
 private void Start()
 {
-anim = GetComponent<Animator>();
-}
-
- // Update is called once per frame
-void Update()
-{
-//Debug.Log("Jump "+isJump);
-if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-        MoveLeft();
-        }
+    anim = GetComponent<Animator>();
+    isCanShoot = false;
+    EnemyController.EnemyKilled = 0;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+    //Debug.Log("Jump "+isJump);
+    if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                MoveLeft();
+            }
     if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-        MoveRight();
-        }
+            {
+            MoveRight();
+            }
     if (Input.GetKeyDown(KeyCode.Space))
-        { Jump();
+        {
+        Jump();
         }
     if (Input.GetKeyUp(KeyCode.RightArrow))
         {
@@ -38,6 +47,11 @@ if (Input.GetKeyDown(KeyCode.LeftArrow))
     if (Input.GetKeyUp(KeyCode.LeftArrow))
         {
         Idle();
+        }
+    
+    if (Input.GetKeyDown(KeyCode.Z))
+        {
+        Fire();
         }
     Move();
     Dead();
@@ -48,17 +62,16 @@ private void OnCollisionStay2D(Collision2D collision)
     // Kondisi ketika menyentuh tanah
 if (isJump)
         {
-       anim.ResetTrigger("jump");
+        anim.ResetTrigger("jump");
         if (idMove == 0) anim.SetTrigger("idle");
         isJump = false;
         }
-    
-}
+    }
 
 private void OnCollisionExit2D(Collision2D collision)
-{
+ {
     // Kondisi ketika menyentuh tanah
-anim.SetTrigger("jump");
+    anim.SetTrigger("jump");
     anim.ResetTrigger("run");
     anim.ResetTrigger("idle");
     isJump = true;
@@ -67,30 +80,29 @@ anim.SetTrigger("jump");
 public void MoveRight()
 {
     idMove = 1;
-    }
+}
 
 public void MoveLeft()
 {
-    idMove = 2;
-    }
+idMove = 2;
+}
 
 private void Move()
 {
-    if (idMove == 1 && !isDead)
-        {
-        // Kondisi ketika bergerak ke kekanan
+if (idMove == 1 && !isDead)
+{
+// Kondisi ketika bergerak ke kekanan
 if (!isJump) anim.SetTrigger("run");
         transform.Translate(1 * Time.deltaTime * 5f, 0, 0);
         transform.localScale = new Vector3(-1f, 1f, 1f);
         }
     if (idMove == 2 && !isDead)
         {
-        // Kondisi ketika bergerak ke kiri
-        
+         // Kondisi ketika bergerak ke kiri
 if (!isJump) anim.SetTrigger("run");
-       transform.Translate(-1 * Time.deltaTime * 5f, 0, 0);
-       transform.localScale = new Vector3(1f, 1f, 1f);
-       }
+        transform.Translate(-1 * Time.deltaTime * 5f, 0, 0);
+        transform.localScale = new Vector3(1f, 1f, 1f);
+        }
     }
 
 public void Jump()
@@ -98,30 +110,43 @@ public void Jump()
 if (!isJump)
 {
 // Kondisi ketika Loncat
-gameObject.GetComponent<Rigidbody2D>().AddForce
-(Vector2.up * 300f);
-        }
-    }
+gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 300f);
+}
+}
 
 private void OnTriggerEnter2D(Collider2D collision)
 {
-    if (collision.transform.tag.Equals("Coin"))
-        {
-        //Data.score += 15;
+if (collision.transform.tag.Equals("Coin"))
+{
+Data.score += 15;
 Destroy(collision.gameObject);
+}
+}
+
+private void OnCollisionEnter2D(Collision2D collision)
+{
+if (collision.transform.tag.Equals("Peluru"))
+{       
+isCanShoot = true;
+}
+    if (collision.transform.tag.Equals("Enemy"))
+        {
+        SceneManager.LoadScene("Game Over");
+        isDead = true;
         }
     }
+
 public void Idle()
 {
-    // kondisi ketika idle/diam
-if (!isJump)
-        {
+    
+       if (!isJump)
+       {
         anim.ResetTrigger("jump");
         anim.ResetTrigger("run");
         anim.SetTrigger("idle");
         }
     idMove = 0;
-    }
+}
 
 private void Dead()
 {
@@ -129,9 +154,39 @@ private void Dead()
         {
         if (transform.position.y < -10f)
             {
-           // kondisi ketika jatuh
+            
 isDead = true;
             }
         }
+    }
+
+void Fire()
+{
+    
+        if (isCanShoot)
+        {
+        //Membuat projectile baru
+GameObject bullet = Instantiate(Projectile, (Vector2)transform.position - projectileOffset * transform.localScale.x, Quaternion.identity);
+       
+        
+Vector2 velocity = new Vector2(projectileVelocity.x * transform.localScale.x, projectileVelocity.y);
+       bullet.GetComponent<Rigidbody2D>().velocity = velocity* -1;
+         
+
+        Vector3 scale = transform.localScale;
+        bullet.transform.localScale = scale * -1;
+        
+        StartCoroutine(CanShoot());
+        anim.SetTrigger("shoot");
+        }
+   }
+
+IEnumerator CanShoot()
+{
+    anim.SetTrigger("shoot");
+    isCanShoot = false;
+    yield return new WaitForSeconds(cooldown);
+    isCanShoot = true;
+    
     }
 }
